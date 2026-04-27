72db836ddb3960576a90bf0543034cb87e518c92
Tracey Kadenyi <treykadenyi@gmail.com>
2026-04-27 15:14:44 +0300

    add role to target user

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -4,7 +4,7 @@ declare global {
   namespace Express {
     interface Request {
       user?: IUser;
-      targetUser?: IUser;
+      targetUser?: IUser | { role: string };
       targetEvent?: IEvent;
       targetExpense?: IExpense;
     }
