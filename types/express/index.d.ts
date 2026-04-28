3a3209cbaf911c1c8dd90e4af0a024c37c3b8b77
Tracey Kadenyi <treykadenyi@gmail.com>
2026-04-28 11:00:47 +0300

    make targetuser IUSER

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -4,7 +4,7 @@ declare global {
   namespace Express {
     interface Request {
       user?: IUser;
-      targetUser?: IUser | { role: string };
+      targetUser?: IUser;
       targetEvent?: IEvent;
       targetExpense?: IExpense;
     }
